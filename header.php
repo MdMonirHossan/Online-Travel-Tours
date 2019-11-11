8726bd8b16859e2d4f693b4dead002b7200573de
MdMonirHossan <mdraju01820@gmail.com>
2019-11-12 00:56:05 +0600

    navbar toggle button

--- header.php
+++ header.php
@@ -16,64 +16,64 @@
 
 
 
-   <nav class="navbar navbar-expand-lg  static-top">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-                <img src="images/logo-full.png" alt="Logo" style="">
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="Packages.php">Packages</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="contact.php">Contact</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">About</a>
-                    </li>
-                    <li>
-                        <div class="dropdown">
-                            <button class="dropbtn">More</button>
-                            <div class="dropdown-content">
-                                <a href="search_booking.php">See Your Booking</a>
-                                <a href="#">Link 2</a>
-                                <a href="#">Link 3</a>
-                            </div>
+<nav class="navbar navbar-expand-lg  static-top">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img src="images/logo-full.png" alt="Logo" style="">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="Packages.php">Packages</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact.php">Contact</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+                <li>
+                    <div class="dropdown">
+                        <button class="dropbtn">More</button>
+                        <div class="dropdown-content">
+                            <a href="search_booking.php">See Your Booking</a>
+                            <a href="#">Link 2</a>
+                            <a href="#">Link 3</a>
                         </div>
-                    </li>
-                   
-                    <?php
-                        // $_SESSION["favcolor"] = "green";
+                    </div>
+                </li>
+                
+                <?php
+                    // $_SESSION["favcolor"] = "green";
 
-                        if( isset( $_SESSION['favcolor'] ) ) {
-                            echo '  <li  class="nav-item">
-                                        <a class="nav-link" href="#">Logout</a>
-                                     </li>';
-                        }
-                        else{
-                            session_unset();
-                            session_destroy();
-                            echo ' 
-                             <li class="nav-item">
-                                 <a class="nav-link" href="login.php">Login</a>
-                            </li>
-                             <li class="nav-item">
-                                 <a class="nav-link" href="signup.php">Sign Up</a>
-                            </li>';
-                        }
-                    ?>
-                </ul>
-            </div>
+                    if( isset( $_SESSION['favcolor'] ) ) {
+                        echo '  <li  class="nav-item">
+                                    <a class="nav-link" href="#">Logout</a>
+                                    </li>';
+                    }
+                    else{
+                        session_unset();
+                        session_destroy();
+                        echo ' 
+                            <li class="nav-item">
+                                <a class="nav-link" href="login.php">Login</a>
+                        </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="signup.php">Sign Up</a>
+                        </li>';
+                    }
+                ?>
+            </ul>
         </div>
-    </nav>
+    </div>
+</nav>
 
 
   
